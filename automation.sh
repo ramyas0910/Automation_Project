82764d4fb5da32d4a772b9c1d7f360aa165cf9f3
root <root@ip-172-31-28-34.ec2.internal>
2022-10-08 21:35:14 +0530

    Added cron job

--- automation.sh
+++ automation.sh
@@ -28,3 +28,25 @@ tar cvf /tmp/ramya-httpd-logs-$timestamp.tar *.log
 
 aws s3 cp /tmp/${myname}-httpd-logs-${timestamp}.tar s3://${s3_bucket}/${myname}-httpd-logs-${timestamp}.tar
 
+if [ ! -f /var/www/html/inventory.html ]
+then
+	        :
+	else
+		        touch /var/www/html/inventory.html
+			        echo -e "Log Type\tDate Created\t Type\tSize" >> /var/www/html/inventory.html
+fi
+
+FILENAME=/root/Automation_Project/automation.sh
+FILESIZE=$(stat -c%s "$FILENAME")
+#echo "Size of $FILENAME = $FILESIZE bytes."
+
+echo -e "httpd-logs\t$timestamp\t tar\t$FILESIZE" >> /var/www/html/inventory.html
+
+if [ ! -f /etc/cron.d/automation ]
+then
+	:
+else
+	touch /etc/cron.d/automation
+	echo -e "0 0 * * * /root/Automation_Project/automation.sh" >> /etc/cron.d/automation
+fi
+
